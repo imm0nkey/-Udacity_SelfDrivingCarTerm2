250d7acbcdadf2c54b83fd8b1c29485ab8347546
imm0nkey <yidingyidingli@hotmail.com>
2021-07-29 15:22:14 +0800

    20210729 restart

--- 04_KalmanFilters/04_41_KalmanMatrices.py
+++ 04_KalmanFilters/04_41_KalmanMatrices.py
@@ -168,7 +168,7 @@ def kalman_filter(x, P):
 
 measurements = [1, 2, 3]
 
-x = matrix([[0.], [0.]])  # initial state (location and velocity
+x = matrix([[0.], [0.]])  # initial state (location and velocity)
 P = matrix([[1000., 0.], [0., 1000.]])  # initial uncertainty
 u = matrix([[0.], [0.]])  # external motion
 F = matrix([[1., 1.], [0., 1.]])  # next state function
